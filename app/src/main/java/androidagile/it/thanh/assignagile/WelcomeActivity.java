05498af7bb3b9d31bfec1abddffbbda39e3acc09
phamKienDev <phamvakien012@gmail.com>
2019-07-22 18:22:43 +0700

    code giaodien by Kien (22/7/2019)

--- app/src/main/java/androidagile/it/thanh/assignagile/WelcomeActivity.java
+++ app/src/main/java/androidagile/it/thanh/assignagile/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package androidagile.it.thanh.assignagile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,26 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        //Dùng cài đặt sau 5 giây màn hình tự chuyển
+        Thread bamgio = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent activitymoi = new Intent(WelcomeActivity.this,MainActivity.class);
+                    startActivity(activitymoi);
+                }
+            }
+        };
+        bamgio.start();
     }
+
+    //sau khi chuyển sang màn hình chính, kết thúc màn hình chào
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
 }
